f0162773dbc5a26df64e10685c16eaf2240da3df
dwiyantech <ahmad2341>
2021-05-15 19:37:04 +0700

    Add ScreenShoot

--- resources/views/vendor/log-viewer/bootstrap-4/_master.blade.php
+++ resources/views/vendor/log-viewer/bootstrap-4/_master.blade.php
@@ -253,6 +253,7 @@
                 LogViewer - <span class="badge badge-info">version {{ log_viewer()->version() }}</span>
             </p>
             <p class="text-muted pull-right">
+                Created with <i class="fa fa-heart"></i> by ARCANEDEV <sup>&copy;</sup>
             </p>
         </div>
     </footer>
